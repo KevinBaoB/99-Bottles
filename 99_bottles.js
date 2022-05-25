1fd8116e9fc41996615661d06e01ee1730b11a2b
KevinBaoB <kevinbaobui@gmail.com>
2022-05-25 16:11:19 -0500

    Corrected

--- 99_bottles.js
+++ 99_bottles.js
@@ -5,9 +5,7 @@ function bottleSong() {
       console.log(`${i} bottles of beer on the wall, ${i} bottles of beer.
       Take one down and pass it around, ${i - 1} bottles of beer on the wall.`);
     } else if ((i = 1)) {
-      console.log(
-        "Take one down and pass it around, 1 bottle of beer on the wall. 1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
-      );
+      return "Take one down and pass it around, 1 bottle of beer on the wall. 1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
     }
   }
 }
